0a85943843a012e10edfd3b42087bb9f730f5b67
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2024-05-10 13:57:40 -0300

    fix: added partners imgs

--- src/components/home/Affiliate.jsx
+++ src/components/home/Affiliate.jsx
@@ -4,16 +4,16 @@ const Affiliate = () => {
         <h2 className="mb-2 font-bold text-2xl">Nuestros Afiliados Deportivos</h2>
         <p>En Portamedallas, nuestra red de afiliados deportivos fortalece nuestro compromiso con el deporte, brindando productos y servicios de calidad respaldados por la experiencia y la dedicación de nuestros socios.</p>
         <div className="py-8 flex flex-wrap gap-x-28 gap-y-4 justify-center">
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
-          <img src="./assets/logo-portamedallas-1.png" alt="logo" width={96}/>
+          <img src="./assets/partners/allianz15k.webp" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/carreraMujer.png" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/ergoFitness.png" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/maratonFlores.png" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/maratonUnicef.png" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/miamiMarathon.png" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/mmb.png" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/mmc.webp" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/runtour.png" alt="logo" className="w-28 object-contain"/>
+          <img src="./assets/partners/correcaminos.png" alt="logo" className="w-28 object-contain"/>
         </div>
     </section>
   )
